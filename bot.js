e2a768d1dfc678f32e086169368845bd961553a7
Tony Nicola <46770472+tdnicola@users.noreply.github.com>
2024-01-27 17:58:51 -0800

    Updated bot.js error checking
    Removed unneeded welcome file. Updated each response with try catch.

--- bot.js
+++ bot.js
@@ -10,8 +10,6 @@ const fs = require("node:fs");
 const path = require("node:path");
 const foldersPath = path.join(__dirname, "commands");
 const commandPath = fs.readdirSync(foldersPath);
-const welcome = require(`./welcome.js`);
-
 
 // Create a new client instance
 const client = new Client({
@@ -29,6 +27,7 @@ client.commands = new Collection();
 for (const file of commandPath) {
     const filePath = path.join(foldersPath, file);
     const command = require(filePath);
+
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     client.commands.set(command.name, command);
 }
@@ -36,13 +35,13 @@ for (const file of commandPath) {
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    welcome(client)
 });
 
 let commandArray = [];
 client.commands.forEach((command) => {
     commandArray.push(command.name);
 });
+
 let commandString = commandArray.join(", ");
 
 const sendToAdmin = (admin, chatMessage) => {
@@ -56,44 +55,55 @@ const sendToAdmin = (admin, chatMessage) => {
     }
 };
 
-
 client.on("messageCreate", (msg) => {
     if (msg.author.bot) return;
+    // Welcome
+    try {
 
-    let isWelcomeMessage = msg.type === 7; //Welcome
-    if (isWelcomeMessage) {
-        let welcome = client.commands.find((file) => file.name == 'Welcome').execute
-        welcome(msg)
-        msg.author.send(`Welcome to the server, ${msg.author.username}!`);
-        client.channels.cache
+        if (msg.type === 7) {
+            let welcome = client.commands.find((file) => file.name == 'Welcome').execute
+            welcome(msg)
+            msg.author.send(`Welcome to the server, ${msg.author.username}!`);
+            client.channels.cache
             .get("513160441187270661") //IphoneGrabbies channel
             // .get("969519390951366676") //InertiaBJJ channel
             .send(`${msg.author.username} has joined the server!`);
-    }
-    if (msg.content.startsWith('!poll') ) {
-        let poll = client.commands.find((file) => file.name == 'Poll').execute
-        poll(msg)
-    }
-    if (!msg.guild) {
-        let wordsMatch = client.commands.size;
-        let noMatch = 0;
-
-        client.commands.forEach((file) => {
-            if (msg.content.toLowerCase().includes(file.name.toLowerCase())) {
-                file.execute(msg);
-            } else {
-                noMatch += 1;
-            }
-        });
-        if (noMatch == wordsMatch) {
-            msg.author.send(`I'm sorry I didn't find a match of my commands. \nPlease try sending me one of the following commands: ${commandString}`);
         }
-        sendToAdmin('254838552960040960',msg);
-        // sendToAdmin('839161115359314020',msg);
+    } catch {
+        sendToAdmin('254838552960040960', `Error in welcome message: ${msg}`);
+    }
+    try {
 
+        if (msg.content.startsWith('!poll') ) {
+            let poll = client.commands.find((file) => file.name == 'Poll').execute
+            poll(msg)
+        }
+    } catch {
+        sendToAdmin('254838552960040960', `Error in Poll message: ${msg}`);
+    }
+    try {
+
+        if (!msg.guild) {
+            let wordsMatch = client.commands.size;
+            let noMatch = 0;
+            
+            client.commands.forEach((file) => {
+                if (msg.content.toLowerCase().includes(file.name.toLowerCase())) {
+                    file.execute(msg);
+                } else {
+                    noMatch += 1;
+                }
+            });
+            if (noMatch == wordsMatch) {
+                msg.author.send(`I'm sorry I didn't find a match of my commands. \nPlease try sending me one of the following commands: ${commandString}`);
+            }
+            sendToAdmin('254838552960040960',msg);
+            // sendToAdmin('839161115359314020',msg);
+        } 
+
+    } catch {
+        sendToAdmin('254838552960040960', `Error in private message command: ${msg}`);
     }
 });
 
-// Testing for DM only
-// Log in to Discord with your client's token
 client.login(token);
